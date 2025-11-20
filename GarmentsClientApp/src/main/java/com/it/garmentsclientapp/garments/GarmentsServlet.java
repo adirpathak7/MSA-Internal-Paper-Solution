4abb25c7921ce81fe82841e964bf37026e14d512
adirpathak7 <adityarpathak7@gmail.com>
2025-11-20 13:16:51 +0530

    docker completed

--- GarmentsClientApp/src/main/java/com/it/garmentsclientapp/garments/GarmentsServlet.java
+++ GarmentsClientApp/src/main/java/com/it/garmentsclientapp/garments/GarmentsServlet.java
@@ -12,6 +12,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.Response;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -43,11 +47,105 @@ public class GarmentsServlet extends HttpServlet {
             out.println("<title>Servlet GarmentsServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>get:- " + garmentsService.get() + "</h1>");
-            out.println("<h1>getAllGarments:- " + garmentsService.getAllGarments()+ "</h1>");
-            out.println("<h1>findGarmentsByByCategory:- " + garmentsService.findGarmentsByByCategory("mens")+ "</h1>");
-//            out.println("<h1>findGarmentsByPrice:- " + garmentsService.findGarmentsByPrice(2100.00)+ "</h1>");
+            out.println("<h1>Garments Data</h1>");
 
+            Response res;
+            res = garmentsService.getAllGarments();
+            if (res != null && res.hasEntity()) {
+                List<?> list = res.readEntity(List.class);
+
+                if (list != null && !list.isEmpty()) {
+                    out.println("<table>");
+                    out.println("<tr><th>GarmentName</th><th>Size</th><th>Category</th><th>Description</th><th>Price</th><th>Stock</th></tr>");
+
+                    for (Object obj : list) {
+                        Map<?, ?> map = (Map<?, ?>) obj;
+                        out.println("<tr>");
+                        out.println("<td>" + map.get("garmentName") + "</td>");
+                        out.println("<td>" + map.get("size") + "</td>");
+                        out.println("<td>" + map.get("category") + "</td>");
+                        out.println("<td>" + map.get("description") + "</td>");
+                        out.println("<td>" + map.get("price") + "</td>");
+                        out.println("<td>" + map.get("stock") + "</td>");
+                        out.println("</tr>");
+                    }
+                    out.println("</table>");
+                } else {
+                    out.println("No data found!");
+                }
+            }
+
+            out.println("<br/>");
+
+            out.println("<h1>Filter garments data</h1>");
+            out.println("<br/>");
+
+            out.println("<form method='get'>");
+            out.println("<h1><b>Filter garments data by category.</b></h1>");
+            out.println("<br/>");
+            out.println("<select name='category'>");
+            out.println("<option value=''>Select the category</option>");
+            out.println("<option value='mens'>Mens</option>");
+            out.println("<option value='womens'>Womens</option>");
+            out.println("<option value='children'>Children</option>");
+            out.println("</select>");
+            out.println("<input type='submit' value='Search'>");
+            out.println("</form>");
+
+            out.println("<br/>");
+
+            out.println("<form method='get'>");
+            out.println("<h1><b>Filter garments data by price.</b></h1>");
+            out.println("<br/>");
+            out.println("<select name='price'>");
+            out.println("<option value=''>Select the price range</option>");
+            out.println("<option value='500-1000'>₹500 - ₹1000</option>");
+            out.println("<option value='1001-1500'>₹1001 - ₹1500</option>");
+            out.println("<option value='1501-99999'>₹1500+</option>");
+            out.println("</select>");
+            out.println("<input type='submit' value='Search'>");
+            out.println("</form>");
+            out.println("<br/>");
+
+            String category = request.getParameter("category");
+            String price = request.getParameter("price");
+
+            if (category != null && !category.isEmpty()) {
+                res = garmentsService.findGarmentsByByCategory(category);
+
+            } else if (price != null && !price.isEmpty()) {
+                String[] values = price.split("-");
+                BigInteger min = new BigInteger(values[0]);
+                BigInteger max = new BigInteger(values[1]);
+                res = garmentsService.findGarmentsByPrice(min, max);
+
+            } else {
+                res = garmentsService.getAllGarments();
+            }
+
+            if (res != null && res.hasEntity()) {
+                List<?> list = res.readEntity(List.class);
+
+                if (list != null && !list.isEmpty()) {
+                    out.println("<table>");
+                    out.println("<tr><th>GarmentName</th><th>Size</th><th>Category</th><th>Description</th><th>Price</th><th>Stock</th></tr>");
+
+                    for (Object obj : list) {
+                        Map<?, ?> map = (Map<?, ?>) obj;
+                        out.println("<tr>");
+                        out.println("<td>" + map.get("garmentName") + "</td>");
+                        out.println("<td>" + map.get("size") + "</td>");
+                        out.println("<td>" + map.get("category") + "</td>");
+                        out.println("<td>" + map.get("description") + "</td>");
+                        out.println("<td>" + map.get("price") + "</td>");
+                        out.println("<td>" + map.get("stock") + "</td>");
+                        out.println("</tr>");
+                    }
+                    out.println("</table>");
+                } else {
+                    out.println("No data found!");
+                }
+            }
             out.println("</body>");
             out.println("</html>");
         }
